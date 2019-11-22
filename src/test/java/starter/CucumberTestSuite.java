955d53a10da536bfbc9993d7f93141e9fe776595
Piyush Panchal <piyush.panchal@sparkventures.co.nz>
2019-11-22 15:42:40 +1300

    minor changes

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -1,15 +1,16 @@
 package starter;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
         tags = "@maven_central_search",
         features = "src/test/resources/features"
 )
 public class CucumberTestSuite {
+
+
 }
